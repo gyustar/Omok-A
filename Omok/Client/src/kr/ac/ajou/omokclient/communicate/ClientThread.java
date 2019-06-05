8dbf0b83b5de3edde691cf7f9635c1451f427a8c
gyustar <gkagudrbbb@gmail.com>
2019-06-05 14:51:02 +0900

    게임룸 기능 추가/ 코드 정리 필

--- Omok/Client/src/kr/ac/ajou/omokclient/communicate/ClientThread.java
+++ Omok/Client/src/kr/ac/ajou/omokclient/communicate/ClientThread.java
@@ -1,7 +1,7 @@
 package kr.ac.ajou.omokclient.communicate;
 
 import com.google.gson.Gson;
-import kr.ac.ajou.omokclient.gui.*;
+import kr.ac.ajou.omokclient.view.*;
 import kr.ac.ajou.omokclient.protoocol.*;
 
 import java.io.*;
@@ -9,23 +9,11 @@ import java.net.Socket;
 
 public class ClientThread extends Thread {
     private Socket socket;
-    private OutputStream os;
-    private DataOutputStream dos;
     private Gson gson;
     private Window window;
-    private int id;
-    private int color;
 
     public ClientThread(Socket socket, Window window) {
         this.socket = socket;
-
-        try {
-            os = socket.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        dos = new DataOutputStream(os);
-
         this.window = window;
         gson = new Gson();
     }
@@ -52,131 +40,10 @@ public class ClientThread extends Thread {
 
                 String json = new String(data, 0, len);
                 Protocol protocol = gson.fromJson(json, Protocol.class);
-                String type = protocol.getType();
-
-                switch (type) {
-                    case "GameStatusData":
-                        GameStatusData gameStatusData =
-                                gson.fromJson(protocol.getData(), GameStatusData.class);
-                        analysisGameStatusData(gameStatusData);
-                        break;
-                    case "IdData":
-                        IdData idData =
-                                gson.fromJson(protocol.getData(), IdData.class);
-                        analysisIdData(idData);
-                        break;
-                    case "PlayerData":
-                        PlayerData playerData =
-                                gson.fromJson(protocol.getData(), PlayerData.class);
-                        analysisPlayerData(playerData);
-                        break;
-                    case "ReadyData":
-                        ReadyData readyData =
-                                gson.fromJson(protocol.getData(), ReadyData.class);
-                        analysisReadyData(readyData);
-                        break;
-                    case "MsgData":
-                        MsgData msgData =
-                                gson.fromJson(protocol.getData(), MsgData.class);
-                        analysisMsgData(msgData);
-                    case "ColorData":
-                        ColorData colorData =
-                                gson.fromJson(protocol.getData(), ColorData.class);
-                        analysisColorData(colorData);
-                        break;
-                    case "TurnData":
-                        TurnData turnData =
-                                gson.fromJson(protocol.getData(), TurnData.class);
-                        analysisTurnData(turnData);
-                        break;
-                    case "StoneData":
-                        StoneData stoneData =
-                                gson.fromJson(protocol.getData(), StoneData.class);
-                        analysisStoneData(stoneData);
-                        break;
-                }
+                window.addQueue(protocol);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private void sendData(Protocol protocol) {
-        try {
-            byte[] data;
-            String json = gson.toJson(protocol);
-            data = json.getBytes();
-            int len = data.length;
-            dos.writeInt(len);
-            os.write(data, 0, len);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void analysisGameStatusData(GameStatusData gameStatusData) {
-        int gameStatus = gameStatusData.getGameStatus();
-        window.setGameStatus(gameStatus);
-    }
-
-    private void analysisIdData(IdData idData) {
-        this.id = idData.getId();
-    }
-
-    private void analysisPlayerData(PlayerData playerData) {
-        int idTemp = playerData.getId();
-        if (idTemp == 0) {
-            window.addPlayer(idTemp, id == idTemp);
-        } else if (idTemp == 1) {
-            window.addPlayer(idTemp - 1, id == idTemp - 1);
-            window.addPlayer(idTemp, id == idTemp);
-        }
-    }
-
-    private void analysisReadyData(ReadyData readyData) {
-        window.readyPlayer(readyData.getReady());
-    }
-
-    private void analysisMsgData(MsgData msgData) {
-        String msg = msgData.getMsg();
-
-        if (msg.equals("Empty"))
-            window.deleteMsgBox();
-        else window.makeMsgBox(new MsgBox(msg));
-    }
-
-    private void analysisColorData(ColorData colorData) {
-        int colorOfPlayer0 = colorData.getColorOfPlayer0();
-        int colorOfPlayer1 = colorData.getColorOfPlayer1();
-        window.setPlayerColor(colorOfPlayer0, colorOfPlayer1);
-
-        if (this.id == 0) color = colorOfPlayer0;
-        else if (this.id == 1) color = colorOfPlayer1;
-    }
-
-    private void analysisTurnData(TurnData turnData) {
-        int turn = turnData.getTurn();
-        window.changeTurn(turn);
-    }
-
-    private void analysisStoneData(StoneData stoneData) {
-        int i = stoneData.getI();
-        int j = stoneData.getJ();
-        int color = stoneData.getColor();
-        window.addStone(new Stone(i, j, color));
-    }
-
-    synchronized public void putStone(int i, int j) {
-        StoneData stoneData = new StoneData(i, j, color);
-        String json = gson.toJson(stoneData);
-        Protocol protocol = new Protocol(json, "StoneData");
-        sendData(protocol);
-    }
-
-    synchronized public void amReady() {
-        ReadyData readyData = new ReadyData(id);
-        String json = gson.toJson(readyData);
-        Protocol protocol = new Protocol(json, "ReadyData");
-        sendData(protocol);
     }
 }
--- Omok/Server/src/kr/ac/ajou/omokserver/communicate/ServerThread.java
+++ Omok/Server/src/kr/ac/ajou/omokserver/communicate/ServerThread.java
@@ -1,34 +1,40 @@
 package kr.ac.ajou.omokserver.communicate;
 
 import com.google.gson.Gson;
-import kr.ac.ajou.omokserver.util.Omok;
 import kr.ac.ajou.omokserver.protocol.*;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static kr.ac.ajou.omokserver.protocol.GameStatusData.*;
+import static kr.ac.ajou.omokserver.protocol.LobbyData.*;
 
 class ServerThread extends Thread {
     private static final int BLACK = 1;
     private static final int WHITE = -1;
     private static final int NONE = 0;
+    private static final int LOBBY = 1010;
+    private static final int ROOM = 1011;
+
     private static final Object MUTEX = new Object();
 
-    private static List<ServerThread> clients = new ArrayList<>();
+    private static List<ServerThread> clients = new CopyOnWriteArrayList<>();
+    private static List<GameRoom> gameRooms = new CopyOnWriteArrayList<>();
     private static int n = 0;
-    private static int readyCount = 0;
+    private static int roomCount = 0;
 
     private Gson gson;
     private Socket socket;
     private OutputStream os;
     private DataOutputStream dos;
     private byte[] data;
-    private Omok omok;
     private int id;
     private int color;
+    private int position;
+    private int roomNumber;
+    private GameRoom gameRoom;
 
     ServerThread(Socket socket) {
         this.socket = socket;
@@ -40,72 +46,99 @@ class ServerThread extends Thread {
         }
         dos = new DataOutputStream(os);
 
-        omok = new Omok();
         gson = new Gson();
         data = new byte[1024];
         color = NONE;
+        position = LOBBY;
 
         synchronized (MUTEX) {
             clients.add(this);
-            id = n++;
+            n++;
+            for (ServerThread t : clients) {
+                if (t.getPosition() == LOBBY)
+                    sendNumOfPlayer();
+            }
+            for (GameRoom room : gameRooms) {
+                RoomInfoData roomInfoData =
+                        new RoomInfoData(room.getRoomNumber(), room.getPlayerCount());
+                String json = gson.toJson(roomInfoData);
+                broadcastLobby(new Protocol(json, "RoomInfoData"));
+            }
         }
-
-        if (n == 1) sendGameStatusData(DEFAULT);
-        else if (n == 2) sendGameStatusData(ALL_ENTER);
-        sendIdData();
-        sendPlayerData();
     }
 
-    private void sendGameStatusData(int gameStatus) {
+    private int getPosition() {
+        return position;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    private void sendNumOfPlayer() {
+        int numOfPlayer;
+        synchronized (MUTEX) {
+            numOfPlayer = n;
+        }
+        LobbyData lobbyData = new LobbyData(NUM_OF_PLAYER, numOfPlayer);
+        String json = gson.toJson(lobbyData);
+        broadcastLobby(new Protocol(json, "LobbyData"));
+    }
+
+    void sendGameStatusData(int gameStatus) {
         GameStatusData gameStatusData = new GameStatusData(gameStatus);
         String json = gson.toJson(gameStatusData);
         Protocol protocol = new Protocol(json, "GameStatusData");
-        broadcast(protocol);
+        broadcastRoom(protocol);
     }
 
-    private void sendIdData() {
+    void sendIdData() {
         IdData idData = new IdData(id);
         String json = gson.toJson(idData);
         Protocol protocol = new Protocol(json, "IdData");
         sendDataToMe(protocol);
     }
 
-    private void sendPlayerData() {
+    void sendPlayerData() {
         PlayerData playerData = new PlayerData(id);
         String json = gson.toJson(playerData);
         Protocol protocol = new Protocol(json, "PlayerData");
-        broadcast(protocol);
+        broadcastRoom(protocol);
     }
 
     private void reset() {
         synchronized (MUTEX) {
-            readyCount = 0;
             for (ServerThread t : clients) {
-                t.omok = new Omok();
                 t.color = NONE;
                 t.sendPlayerData();
             }
         }
 
         GameStatusData gameStatusData = new GameStatusData(ALL_ENTER);
-        broadcast(new Protocol(gson.toJson(gameStatusData), "GameStatusData"));
+        broadcastRoom(new Protocol(gson.toJson(gameStatusData), "GameStatusData"));
     }
 
-    private void clientExit() {
-        synchronized (MUTEX) {
-            n = clients.size();
-            readyCount = 0;
-            for (ServerThread t : clients) {
-                t.omok = new Omok();
-                t.id = 0;
-                t.color = NONE;
-                t.sendIdData();
-                t.broadcast(new Protocol(gson.toJson(new GameStatusData(RESET)),
-                        "GameStatusData"));
-                t.broadcast(new Protocol(gson.toJson(new GameStatusData(DEFAULT)),
-                        "GameStatusData"));
-                t.sendPlayerData();
+    private void clientExit(ServerThread serverThread) {
+        n = clients.size();
+        sendNumOfPlayer();
+        if (serverThread.position == ROOM) {
+            synchronized (MUTEX) {
+                for (ServerThread t : clients) {
+                    if (t.position == ROOM && t.roomNumber == this.roomNumber) {
+                        t.color = NONE;
+                        t.sendDataToMe(new Protocol(gson.toJson(new GameStatusData(RESET)),
+                                "GameStatusData"));
+                        break;
+                    }
+                }
             }
+
+            gameRoom.exitPlayer(serverThread);
+
+            RoomInfoData roomInfoData =
+                    new RoomInfoData(roomNumber, gameRoom.getPlayerCount());
+            String json = gson.toJson(roomInfoData);
+            broadcastLobby(new Protocol(json, "RoomInfoData"));
         }
     }
 
@@ -122,15 +155,55 @@ class ServerThread extends Thread {
                     t.os.write(data, 0, len);
                 } catch (IOException e) {
                     clients.remove(t);
-                    clientExit();
+                    clientExit(t);
+                }
+            }
+        }
+    }
+
+    private void broadcastRoom(Protocol protocol) {
+        String json = gson.toJson(protocol);
+        data = json.getBytes();
+        int len = data.length;
+        synchronized (MUTEX) {
+            for (int i = 0; i < clients.size(); ++i) {
+                ServerThread t = clients.get(i);
+                if (t.position == ROOM && t.roomNumber == this.roomNumber) {
+                    try {
+                        t.dos.writeInt(len);
+                        t.os.write(data, 0, len);
+                    } catch (IOException e) {
+                        clients.remove(t);
+                        clientExit(t);
+                    }
+                }
+            }
+        }
+    }
+
+    private void broadcastLobby(Protocol protocol) {
+        String json = gson.toJson(protocol);
+        data = json.getBytes();
+        int len = data.length;
+        synchronized (MUTEX) {
+            for (int i = 0; i < clients.size(); ++i) {
+                ServerThread t = clients.get(i);
+                if (t.position == LOBBY) {
+                    try {
+                        t.dos.writeInt(len);
+                        t.os.write(data, 0, len);
+                    } catch (IOException e) {
+                        clients.remove(t);
+                        clientExit(t);
+                    }
                 }
             }
         }
     }
 
     private void sendDataToMe(Protocol protocol) {
-        String str = gson.toJson(protocol);
-        data = str.getBytes();
+        String json = gson.toJson(protocol);
+        data = json.getBytes();
         int len = data.length;
 
         try {
@@ -139,7 +212,7 @@ class ServerThread extends Thread {
         } catch (IOException e) {
             synchronized (MUTEX) {
                 clients.remove(this);
-                clientExit();
+                clientExit(this);
             }
         }
     }
@@ -178,7 +251,7 @@ class ServerThread extends Thread {
                 } catch (EOFException e) {
                     synchronized (MUTEX) {
                         clients.remove(this);
-                        clientExit();
+                        clientExit(this);
                     }
                     break;
                 }
@@ -193,6 +266,11 @@ class ServerThread extends Thread {
                 String type = protocol.getType();
 
                 switch (type) {
+                    case "LobbyData":
+                        LobbyData lobbyData =
+                                gson.fromJson(protocol.getData(), LobbyData.class);
+                        analysisLobbyData(lobbyData);
+                        break;
                     case "ReadyData":
                         ReadyData readyData =
                                 gson.fromJson(protocol.getData(), ReadyData.class);
@@ -209,106 +287,147 @@ class ServerThread extends Thread {
             synchronized (MUTEX) {
                 clients.remove(this);
             }
-            clientExit();
+            clientExit(this);
+        }
+    }
+
+    private void analysisLobbyData(LobbyData lobbyData) {
+        short header = lobbyData.getHeader();
+        if (header == CREATE_ROOM) {
+            int roomNumberTemp;
+            synchronized (MUTEX) {
+                roomNumberTemp = roomCount++;
+            }
+            lobbyData = new LobbyData(ENTER_ROOM, roomNumberTemp);
+            String json = gson.toJson(lobbyData);
+            sendDataToMe(new Protocol(json, "LobbyData"));
+
+            position = ROOM;
+            roomNumber = roomNumberTemp;
+            gameRoom = new GameRoom(roomNumberTemp);
+            gameRooms.add(gameRoom);
+            gameRoom.enterPlayer(this);
+
+            RoomInfoData roomInfoData = new RoomInfoData(roomNumberTemp, 1);
+            json = gson.toJson(roomInfoData);
+            broadcastLobby(new Protocol(json, "RoomInfoData"));
+        } else if (header == ENTER_ROOM) {
+            position = ROOM;
+            roomNumber = lobbyData.getRoomNumber();
+            gameRoom = gameRooms.get(roomNumber);
+            gameRoom.enterPlayer(this);
+
+            lobbyData = new LobbyData(ENTER_ROOM, roomNumber);
+            String json = gson.toJson(lobbyData);
+            sendDataToMe(new Protocol(json, "LobbyData"));
+
+            RoomInfoData roomInfoData =
+                    new RoomInfoData(roomNumber, gameRoom.getPlayerCount());
+            json = gson.toJson(roomInfoData);
+            broadcastLobby(new Protocol(json, "RoomInfoData"));
+
+            PlayerData playerData = new PlayerData(this.id);
+            json = gson.toJson(playerData);
+            broadcastRoom(new Protocol(json, "PlayerData"));
         }
     }
 
     private void analysisReadyData(ReadyData readyData) {
         String json = gson.toJson(readyData);
-        broadcast(new Protocol(json, "ReadyData"));
+        broadcastRoom(new Protocol(json, "ReadyData"));
 
-        synchronized (MUTEX) {
-            readyCount++;
-            if (readyCount == 2) {
-                long start = System.currentTimeMillis();
-                long end = start;
+        gameRoom.playerReady();
+        if (gameRoom.isAllReady()) {
+            long start = System.currentTimeMillis();
+            long end = start;
 
-                MsgData msgData = new MsgData("3");
-                broadcast(new Protocol(gson.toJson(msgData), "MsgData"));
+            MsgData msgData = new MsgData("3");
+            broadcastRoom(new Protocol(gson.toJson(msgData), "MsgData"));
 
-                while ((end - start) < 1000.0) {
-                    end = System.currentTimeMillis();
-                }
+            while ((end - start) < 1000.0) {
+                end = System.currentTimeMillis();
+            }
 
-                start = System.currentTimeMillis();
-                end = start;
-                msgData = new MsgData("2");
-                broadcast(new Protocol(gson.toJson(msgData), "MsgData"));
-                while ((end - start) < 1000.0) {
-                    end = System.currentTimeMillis();
-                }
+            start = System.currentTimeMillis();
+            end = start;
+            msgData = new MsgData("2");
+            broadcastRoom(new Protocol(gson.toJson(msgData), "MsgData"));
+            while ((end - start) < 1000.0) {
+                end = System.currentTimeMillis();
+            }
 
-                start = System.currentTimeMillis();
-                end = start;
-                msgData = new MsgData("1");
-                broadcast(new Protocol(gson.toJson(msgData), "MsgData"));
-                while ((end - start) < 1000.0) {
-                    end = System.currentTimeMillis();
-                }
+            start = System.currentTimeMillis();
+            end = start;
+            msgData = new MsgData("1");
+            broadcastRoom(new Protocol(gson.toJson(msgData), "MsgData"));
+            while ((end - start) < 1000.0) {
+                end = System.currentTimeMillis();
+            }
 
-                start = System.currentTimeMillis();
-                end = start;
-                msgData = throwDice();
-                broadcast(new Protocol(gson.toJson(msgData), "MsgData"));
-                while ((end - start) < 2000.0) {
-                    end = System.currentTimeMillis();
-                }
+            start = System.currentTimeMillis();
+            end = start;
+            msgData = throwDice();
+            broadcastRoom(new Protocol(gson.toJson(msgData), "MsgData"));
+            while ((end - start) < 2000.0) {
+                end = System.currentTimeMillis();
+            }
 
-                msgData = new MsgData("Empty");
-                broadcast(new Protocol(gson.toJson(msgData), "MsgData"));
+            msgData = new MsgData("Empty");
+            broadcastRoom(new Protocol(gson.toJson(msgData), "MsgData"));
 
-                GameStatusData gameStatusData = new GameStatusData(RUNNING);
-                broadcast(new Protocol(gson.toJson(gameStatusData), "GameStatusData"));
+            GameStatusData gameStatusData = new GameStatusData(RUNNING);
+            broadcastRoom(new Protocol(gson.toJson(gameStatusData), "GameStatusData"));
 
-                ColorData colorData = null;
-                TurnData turnData = null;
+            ColorData colorData = null;
+            TurnData turnData = null;
 
-                if (id == 0) {
-                    colorData = new ColorData(color, -color);
-                    if (color == BLACK) turnData = new TurnData(0);
-                    else if (color == WHITE) turnData = new TurnData(1);
-                } else if (id == 1) {
-                    colorData = new ColorData(-color, color);
-                    if (color == BLACK) turnData = new TurnData(1);
-                    else if (color == WHITE) turnData = new TurnData(0);
-                }
-                try {
-                    broadcast(new Protocol(gson.toJson(colorData), "ColorData"));
-                    broadcast(new Protocol(gson.toJson(turnData), "TurnData"));
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                }
+            if (id == 0) {
+                colorData = new ColorData(color, -color);
+                if (color == BLACK) turnData = new TurnData(0);
+                else if (color == WHITE) turnData = new TurnData(1);
+            } else if (id == 1) {
+                colorData = new ColorData(-color, color);
+                if (color == BLACK) turnData = new TurnData(1);
+                else if (color == WHITE) turnData = new TurnData(0);
+            }
+            try {
+                broadcastRoom(new Protocol(gson.toJson(colorData), "ColorData"));
+                broadcastRoom(new Protocol(gson.toJson(turnData), "TurnData"));
+            } catch (NullPointerException e) {
+                e.printStackTrace();
             }
         }
     }
 
     private void analysisStoneData(StoneData stoneData) {
-        broadcast(new Protocol(gson.toJson(stoneData), "StoneData"));
+        broadcastRoom(new Protocol(gson.toJson(stoneData), "StoneData"));
 
         int i = stoneData.getI();
         int j = stoneData.getJ();
         int color = stoneData.getColor();
-        omok.putStone(i, j, color);
 
-        if (omok.winCheck(i, j)) {
+        gameRoom.putStone(i, j, color);
+
+        if (gameRoom.winCheck(i, j)) {
             long start = System.currentTimeMillis();
             long end = start;
 
             String msg = "P" + id + " win!!";
             MsgData msgData = new MsgData(msg);
-            broadcast(new Protocol(gson.toJson(msgData), "MsgData"));
+            broadcastRoom(new Protocol(gson.toJson(msgData), "MsgData"));
 
             while ((end - start) < 3000.0) {
                 end = System.currentTimeMillis();
             }
 
             GameStatusData gameStatusData = new GameStatusData(RESET);
-            broadcast(new Protocol(gson.toJson(gameStatusData), "GameStatusData"));
+            broadcastRoom(new Protocol(gson.toJson(gameStatusData), "GameStatusData"));
 
+            gameRoom.resetGame();
             reset();
         } else {
             TurnData turnData = new TurnData((id * -1) + 1);
-            broadcast(new Protocol(gson.toJson(turnData), "TurnData"));
+            broadcastRoom(new Protocol(gson.toJson(turnData), "TurnData"));
         }
     }
 }
